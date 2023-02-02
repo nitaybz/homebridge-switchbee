61002fcde9b68fee3a9e7640e5c08cf6017740d5
Nitay Ben Zvi <nitaybz@gmail.com>
2023-02-02 18:34:54 +0200

    fix for IR

--- homekit/IR.js
+++ homekit/IR.js
@@ -70,9 +70,9 @@ class Switch {
 
 	addSwitchService(code) {
 		code.name = code.name.replace("+", "plus").replace(/[^\w\s]/ig, "_")
-		this.SwitchServices[code.value] = this.accessory.getService(code.value)
+		this.SwitchServices[code.value] = this.accessory.getService(code.name+code.value)
 		if (!this.SwitchServices[code.value])
-			this.SwitchServices[code.value] = this.accessory.addService(Service.Switch, code.name, code.value)
+			this.SwitchServices[code.value] = this.accessory.addService(Service.Switch, code.name, code.name+code.value)
 
 		this.log(`Adding New IR Command "${code.name}" to : "${this.name}" (id:${this.id})`)
 		this.SwitchServices[code.value].getCharacteristic(Characteristic.On)
